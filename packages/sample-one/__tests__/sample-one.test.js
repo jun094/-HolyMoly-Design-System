f31b36f7b41e676312631cd9537653042cbdb060
dev-jun <42564107+jun094@users.noreply.github.com>
2022-04-04 00:17:06 +0900

    feat: create subpackages

---
+++ packages/sample-one/__tests__/sample-one.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const sampleOne = require('..');
+
+describe('sample-one', () => {
+    it('needs tests');
+});
---
+++ packages/sample-two/__tests__/sample-two.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const sampleTwo = require('..');
+
+describe('sample-two', () => {
+    it('needs tests');
+});
